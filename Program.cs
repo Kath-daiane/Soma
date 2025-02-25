0d7f8cea5bc8e147c7d206cf2f9a63e15af6f02a
Kath-daiane <kath.daiane.p@gmail.com>
2025-02-25 11:26:05 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número: ");
+        int valor1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite outro número: ");
+        int valor2 = int.Parse(Console.ReadLine());
+
+        int soma = valor1 + valor2;
+
+        Console.WriteLine($"Soma = {soma}");
+    }
+}
